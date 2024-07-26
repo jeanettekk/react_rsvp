d5393a2f8f2ac5a81657cb7bf3e9d153182b9897
Jeanette <khojeanette@gmail.com>
2024-07-26 15:02:41 +0100

    Fixed repo name

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/repository/', // replace 'repository' with your repo name
+  base: '/react_rsvp/', // replace 'repository' with your repo name
 });
