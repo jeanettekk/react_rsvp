89ede2ec4e97bac985768628157e0824d6b9c40c
Jeanette <khojeanette@gmail.com>
2024-07-26 14:40:58 +0100

    Adding initial files

---
+++ src/components/HomeBanner.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import couple from '../assets/images/couple.jpg';
+
+const HomeBanner = () => {
+  return (
+    <div className="bg-image">
+        <img className="mw-100" src={couple} alt="Couple"/>
+    </div>
+  )
+}
+
+export default HomeBanner
