1ce78718526c0bf275046e414988b9e63214ab58
Loqorocco <muhdluqmannn@gmail.com>
2024-08-20 20:05:05 +0100

    Center navbar

--- src/components/AppNavbar.jsx
+++ src/components/AppNavbar.jsx
@@ -1,46 +1,48 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
+import { Navbar, Nav } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const NavbarContainer = styled(Navbar)`
-  position: absolute; /* Change from 'fixed' to 'absolute' */
-  top: 0; /* Adjust as needed for your layout */
-  z-index: 1000; /* Ensure it appears above other content */
+  position: absolute; /* Keeps the navbar at the top */
+  top: 0; 
+  width: 100%; /* Ensure it spans the full width of the page */
+  z-index: 1000; 
+  display: flex;
+  justify-content: center; /* Center everything within the navbar */
+  align-items: center;
+`;
+
+const NavItemsContainer = styled(Nav)`
+  display: flex;
+  justify-content: space-around; /* Space items evenly within this container */
+  align-items: center;
 `;
 
 const Title = styled.h1`
   font-size: 2.2rem;
   color: white;
+  margin: 0 2rem; /* Add some margin to keep it separated from the links */
 `;
 
 const StyledNavLink = styled(Nav.Link)`
   font-size: 1.2rem;
   color: white;
-
+  margin: 0 1rem; /* Adjust spacing between nav links */
 `;
 
 const AppNavbar = () => {
   return (
     <NavbarContainer className="p-3" expand="lg">
-      <Navbar.Brand>
-        <Title className="px-5" href="#home">Rhys & Tenny</Title>
-      </Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="ml-auto">
-          <StyledNavLink href="#about">About Us</StyledNavLink>
-          <StyledNavLink href="#bridesmaids">Bridesmaids</StyledNavLink>
-          <StyledNavLink href="#groomsmen">Groomsmen</StyledNavLink>
-          <StyledNavLink href="#schedule">Schedule</StyledNavLink>
-          <StyledNavLink href="#location">Location</StyledNavLink>
-          {/* <NavDropdown title="More" id="basic-nav-dropdown">
-            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-          </NavDropdown> */}
-        </Nav>
-      </Navbar.Collapse>
+      <NavItemsContainer>
+        <StyledNavLink href="#about">About Us</StyledNavLink>
+        <StyledNavLink href="#schedule">Schedule</StyledNavLink>
+        <StyledNavLink href="#groomsmen">Groomsmen</StyledNavLink>
+        <Title href="#home">Rhys & Teniola</Title>
+        <StyledNavLink href="#bridesmaids">Bridesmaids</StyledNavLink>
+        <StyledNavLink href="#RSVP">RSVP</StyledNavLink>
+        <StyledNavLink href="#location">Location</StyledNavLink>
+      </NavItemsContainer>
     </NavbarContainer>
   );
 };
