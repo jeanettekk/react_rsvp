14aa693126e24797d3acef771b5966abfd290be2
Loqorocco <muhdluqmannn@gmail.com>
2024-10-19 16:42:24 +0100

    Added new pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,40 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+
 import AppNavbar from './components/AppNavbar';
 import HomeBanner from './components/HomeBanner';
 import WelcomeMessage from './components/WelcomeMessage';
+import Home from './pages/Home';  // Home component
+import AboutUs from './pages/AboutUs';
+import Schedule from './pages/Schedule';
 
 const App = () => {
   return (
     <>
-      <AppNavbar />
-      <HomeBanner />
-      <WelcomeMessage />
+      <AppNavbar />  {/* Navbar visible on all routes */}
+
+      <div>
+        <Routes>
+          {/* Home route with HomeBanner */}
+          <Route 
+            path="/" 
+            element={
+              <>
+                <HomeBanner />  {/* HomeBanner only appears on the home page */}
+                <Home />
+              </>
+            } 
+          />
+
+          {/* About Us route */}
+          <Route path="/about" element={<AboutUs />} />
+
+          {/* Schedule route */}
+          <Route path="/schedule" element={<Schedule />} />
+        </Routes>
+      </div>
     </>
   );
 };
 
-export default App
+export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import WelcomeMessage from '../components/WelcomeMessage';  // Import WelcomeMessage
+
+const Home = () => {
+  return (
+    <div>
+      <WelcomeMessage />
+      {/* You can add more content specific to the home page here */}
+    </div>
+  );
+};
+
+export default Home;
