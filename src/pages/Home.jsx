6cd004efaf08cdc6d41243123ee0d13c11824dba
Jeanette Kho <khojeanette@gmail.com>
2024-12-01 21:36:04 +0000

    Added address to homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,52 +1,68 @@
 import React from 'react';
-import { styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
-import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
 import couple from '../assets/images/couple.jpg';
 
-const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: '#fff',
-  ...theme.typography.body2,
-  textAlign: 'center',
-  boxShadow: 'none',
-  color: theme.palette.text.secondary,
-  ...theme.applyStyles('dark', {
-    backgroundColor: '#1A2027',
-  }),
-}));
-
 const Home = () => {
   return (
-    <Box sx={{ flexGrow: 1, minHeight: '100vh', width: '100vw', overflowY: 'auto'}}>
-      <Grid container spacing={2}>
-
-        <Grid item sm={12} md={6} 
-          sx={{ 
-            order: { xs: 2, sm: 2, md: 1}, 
-            display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' 
+    <Box sx={{ flexGrow: 1, height: '100vh', width: '100vw', overflowY: 'auto' }}>
+      <Grid container spacing={2} sx={{ height: '100vh' }}>
+        {/* Left Section */}
+        <Grid
+          item
+          sm={12}
+          md={6}
+          sx={{
+            order: { xs: 2, md: 1 },
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
           }}
         >
-          <Item style={{ width: '100%' }}>
-            <h1 style={{ margin: 0, fontSize: '2.5rem', color: '#333' }}>
+          <Box
+            sx={{
+              textAlign: 'center',
+              padding: '50px 10px 50px 10px'
+            }}
+          >
+            <h1 style={{ margin: 0, fontSize: '2.5rem', color: '#F4504C' }}>
               We're getting married!
             </h1>
-          </Item>
-
+            <h2 style={{padding: '20px'}}>February 26, 2026, Thursday</h2>
+            <h5>
+              St Marks Church
+              <br />
+              82 Lincoln Rd, Peterborough
+              <br />
+              PE1 2SN
+            </h5>
+          </Box>
         </Grid>
 
-        <Grid item sm={12} md={6} sx={{ order: { xs: 1, sm: 1, md: 2 }, height: '100vh' }}>
-
-          <Item style={{ height: '100vh', width: '100%'}}>
-            <img 
-              src={couple} 
-              alt="Couple" 
-              style={{ width: '100%', height: '100%', maxHeight: '100vh', objectFit: 'cover', objectPosition: '50% 5%', display: 'block' }} 
+        {/* Right Section */}
+        <Grid
+          item
+          sm={12}
+          md={6}
+          sx={{
+            order: { xs: 1, md: 2 },
+            height: '100vh',
+          }}
+        >
+          <Box sx={{ height: '100%', width: '100%' }}>
+            <img
+              src={couple}
+              alt="Couple"
+              style={{
+                width: '100%',
+                height: '100vh',
+                objectFit: 'cover',
+                objectPosition: '50% 5%',
+                display: 'block',
+              }}
             />
-          </Item>
-
+          </Box>
         </Grid>
-
       </Grid>
     </Box>
   );
